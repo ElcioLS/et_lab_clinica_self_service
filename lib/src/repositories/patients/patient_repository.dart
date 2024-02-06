d081b27e3c377ae9b9497fde5cdf914a140d4a46
ElcioLS <elciols@gmail.com>
2024-02-06 20:50:03 -0300

    data, model & repositories ok

--- lib/src/repositories/patients/patient_repository.dart
+++ lib/src/repositories/patients/patient_repository.dart
@@ -3,6 +3,6 @@ import 'package:et_lab_clinica_core/et_lab_clinica_core.dart';
 import '../../models/patient_model.dart';
 
 abstract interface class PatientRepository {
-  Future<Either<RepositoryException, PatientModel>> findPatientByDocument(
+  Future<Either<RepositoryException, PatientModel?>> findPatientByDocument(
       String document);
 }
---
+++ lib/src/repositories/patients/patient_repository_impl.dart
@@ -0,0 +1,35 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:et_lab_clinica_core/et_lab_clinica_core.dart';
+
+import 'package:et_lab_clinica_self_service/src/models/patient_model.dart';
+
+import './patient_repository.dart';
+
+class PatientRepositoryImpl implements PatientRepository {
+  PatientRepositoryImpl({
+    required this.restClient,
+  });
+
+  final RestClient restClient;
+
+  @override
+  Future<Either<RepositoryException, PatientModel?>> findPatientByDocument(
+      String document) async {
+    try {
+      final Response(:List data) =
+          await restClient.auth.get('/patients', queryParameters: {
+        'document': document,
+      });
+
+      if (data.isEmpty) {
+        return Right(null);
+      }
+      return Right(PatientModel.fromJson(data.first));
+    } on DioException catch (e, s) {
+      log('Erro ao buscar paciente por CPF', error: e, stackTrace: s);
+      return Left(RepositoryException());
+    }
+  }
+}
