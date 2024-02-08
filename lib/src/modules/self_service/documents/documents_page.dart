a653d4c73fa260143688ca049a0265f38d65d6bf
ElcioLS <elciols@gmail.com>
2024-02-08 20:42:15 -0300

    Days4-class1

--- lib/src/modules/self_service/documents/documents_page.dart
+++ lib/src/modules/self_service/documents/documents_page.dart
@@ -1,15 +1,99 @@
+import 'package:et_lab_clinica_core/et_lab_clinica_core.dart';
+import 'package:et_lab_clinica_self_service/src/modules/self_service/documents/widgets/document_box_widget.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/lab_clinica_self_service_app_bar.dart';
 
 class DocumentsPage extends StatelessWidget {
   const DocumentsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Documentos'),
+      appBar: LabClinicaSelfServiceAppBar(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: SingleChildScrollView(
+          child: Container(
+              width: sizeOf.width * .85,
+              margin: const EdgeInsets.only(top: 18),
+              padding: const EdgeInsets.all(32),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(color: LabClinicaTheme.orangeColor),
+              ),
+              child: Column(
+                children: [
+                  Image.asset('assets/images/folder.png'),
+                  const SizedBox(height: 24),
+                  const Text(
+                    'ADICIONAR DOCUMENTOS',
+                    style: LabClinicaTheme.titleSmallStyle,
+                  ),
+                  const SizedBox(height: 32),
+                  const Text(
+                    'Selecione o documento que deseja fotografar',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: LabClinicaTheme.blueColor,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  SizedBox(
+                    width: sizeOf.width * .8,
+                    height: 300,
+                    child: Row(
+                      children: [
+                        DocumentBoxWidget(
+                          uploaded: true,
+                          icon: Image.asset('assets/images/id_card.png'),
+                          label: 'CARTEIRINHA',
+                          totalFiles: 1,
+                        ),
+                        const SizedBox(width: 32),
+                        DocumentBoxWidget(
+                          uploaded: false,
+                          icon: Image.asset('assets/images/document.png'),
+                          label: 'PEDIDO MÉDICO',
+                          totalFiles: 4,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            foregroundColor: Colors.red,
+                            side: const BorderSide(color: Colors.red),
+                            fixedSize: const Size.fromHeight(48),
+                          ),
+                          onPressed: () {},
+                          child: const Text('REMOVER TODAS'),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: LabClinicaTheme.orangeColor,
+                            fixedSize: const Size.fromHeight(48),
+                          ),
+                          onPressed: () {},
+                          child: const Text('FINALIZAR'),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )),
+        ),
       ),
-      body: Container(),
     );
   }
 }
